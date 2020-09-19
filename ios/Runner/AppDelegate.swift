2d04d42a416df457d08e565bb2cd151ae953eb3d
TokinNik <TokinNik@yandex.ru>
2020-09-20 01:38:01 +0700

    Карта и установка маркеров (без сохранения)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyDFbZF-KlOiyNzv3Q4pFRlF76-l7Pn96Xc")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
